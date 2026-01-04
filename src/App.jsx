d9eae8a05362ecc7cdfcb961959c837bb78d42ad
Alifiideas <hmsoaib996@gmail.com>
2026-01-04 19:05:43 +0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Pricing from "./components/Pricing";
+import FAQ from "./components/FAQ";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Pricing />
+      <FAQ />
+    </>
+  );
+}
